e163f3cd9497b43db5e2f732b97a493e980b92ca
Saqib <ranamuhammadsaqib25@gmail.com>
2020-04-28 00:08:41 +0500

    MESSAGE

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Google_Client;
+use Google_Service_Classroom;
+
+class LoginController extends Controller
+{
+	public function login()
+	{
+		$client = new Google_Client();
+		$client->setScopes(Google_Service_Classroom::CLASSROOM_COURSES_READONLY);
+		$client->setAccessType('offline');
+		$client->setPrompt('select_account consent');
+		$authUrl = $client->createAuthUrl();
+		print_r($authUrl);
+
+	}
+}
---
+++ config/services.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+    'google' => [
+        'client_id'     => env('317224244604-73du2bv4aam48jovaraoe1ok7bj3f0ug.apps.googleusercontent.com'),
+        'client_secret' => env('-97l7s_e0l84XU6VPwkCGZZ0'),
+        'redirect'      => env('g-classroom.virgoworlds.com/googleouthcallback')
+    ],
+
+];
